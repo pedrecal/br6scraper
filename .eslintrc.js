d661a1d6286b24eaf4a42534a098b11e174c031d
Alexandre Pedrecal <pedrecalc@gmail.com>
2019-01-14 20:30:37 -0300

    It works... ESLint added

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "extends": "airbnb-base",
+  rules: {
+    'no-console': "off",
+    'no-plusplus': [2, { allowForLoopAfterthoughts: true }],
+    'no-shadow': "off",
+    'quotes': [2, "single"],
+    'prefer-const': "off",
+    'eslint-env': "es6"
+  }
+};
--- br6scraper.js
+++ br6scraper.js
@@ -1,6 +1,7 @@
 const rp = require('request-promise');
 const $ = require('cheerio');
 const moment = require('moment');
+
 const url = 'https://liquipedia.net/rainbowsix/Brasileir%C3%A3o/2019/Regular_Season'; //! Separar ano
 
 const pathToNames = '.grouptableslot > .team-template-team-standard > .team-template-text > a';
@@ -15,16 +16,14 @@ const getTeams = async (url, path) => {
     const teams = [];
     const aux = ($(path, html).not('.bracket-popup').length);
     for (let i = 0; i < aux; i++) {
-      teams.push($(path, html)[i].children[0].data.trim())
+      teams.push($(path, html)[i].children[0].data.trim());
     }
     return teams;
-  }
-  catch (error) {
+  } catch (error) {
     return Promise.reject(error);
   }
-}
+};
 
-//! Fazer dicionário no formato: Round:Date
 const getMatchDates = async (url, path) => {
   try {
     const html = await rp(url);
@@ -34,11 +33,10 @@ const getMatchDates = async (url, path) => {
       dates.push(moment($(path, html)[i].children[0].data.trim(), 'MMMM DD, YYYY').format('DD-MM-YYYY'));
     }
     return dates;
-  }
-  catch (error) {
+  } catch (error) {
     return Promise.reject(error);
   }
-}
+};
 
 const getMatches = async (url, path) => {
   try {
@@ -46,14 +44,13 @@ const getMatches = async (url, path) => {
     const matches = [];
     const aux = ($(path, html).length);
     for (let i = 0; i < aux; i++) {
-      matches.push($(path, html)[i].children[0].data.trim())
+      matches.push($(path, html)[i].children[0].data.trim());
     }
     return matches;
-  }
-  catch (error) {
+  } catch (error) {
     return Promise.reject(error);
   }
-}
+};
 
 const getScores = async (url, path1, path2) => {
   try {
@@ -61,37 +58,46 @@ const getScores = async (url, path1, path2) => {
     const scores = [];
     const aux = ($(path1, html).length);
     for (let i = 0; i < aux; i++) {
-      scores.push(parseInt($(path1, html)[i].children[0].data.trim()));
-      scores.push(parseInt($(path2, html)[i].children[0].data.trim()));
+      scores.push(parseInt($(path1, html)[i].children[0].data.trim(), 10));
+      scores.push(parseInt($(path2, html)[i].children[0].data.trim(), 10));
     }
     return scores;
-  }
-  catch (error) {
+  } catch (error) {
     return Promise.reject(error);
   }
-}
+};
 
+//! Don't look at this, its a monstrosity
 const getRounds = async () => {
-  let dates = await getMatchDates(url, pathToDates);
-  let matches = await getMatches(url, pathToMatches);
-  let scores = await getScores(url, pathToScores1, pathToScores2);
+  const dates = await getMatchDates(url, pathToDates);
+  const matches = await getMatches(url, pathToMatches);
+  const scores = await getScores(url, pathToScores1, pathToScores2);
 
-  // for (let i = 0; i < 1/*(dates.length / 2)*/; i++) {
-    var myJSON = {
-      "match": {
-        "date": dates[0],
-        "team": matches[0],
-        "score": scores[0],
-        "team1": matches[1],
-        "score1": scores[1],
-      }
+  const rounds = [];
+
+  let i = 0;
+  let j = 0;
+  for (; j < matches.length; j++) {
+    if (i !== 0) {
+      (j % 4 ? i : i++);
     }
-  // }
-  console.log(myJSON);
+
+    const round = {
+      match: {
+        date: dates[i],
+        team: matches[j],
+        score: scores[j],
+        team_adversary: matches[j += 1],
+        score_adversary: scores[j],
+      },
+    };
+    rounds.push(round);
+  }
+  console.log(rounds);
+  // console.log(rounds.length);
   // var myString = JSON.stringify(myJSON);
   // console.log(myString);
-
-}
+};
 
 const main = async () => {
   // let teams = await getTeams(url, pathToNames);
@@ -101,7 +107,6 @@ const main = async () => {
   // console.log(matches);
   // let scores = await getScores(url, pathToScores1, pathToScores2);
   getRounds();
-  // parseDate();
-}
+};
 
 main();
